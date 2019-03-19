94bc893692d479de877b61698b6e33f1a2f77899
ffellie <ffellie@github.com>
2019-03-19 12:04:58 +0600

    19.03

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -3,13 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 import { InfoComponent } from './components/info/info.component';
 import { NewsComponent } from './components/news/news.component';
-import { ClassComponent } from './components/class/class.component';
 
 const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'info', component: InfoComponent},
-  { path: 'news', component: NewsComponent},
-  { path: 'class', component: ClassComponent}
+  { path: 'news', component: NewsComponent}
 
 ];
 
