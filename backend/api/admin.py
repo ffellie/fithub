b0a9db4fdfc948baa1612e5de50958e47f4563a8
sarsembin <31287515+sarsembin@users.noreply.github.com>
2019-05-08 12:22:26 +0600

    Update admin.py

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -4,3 +4,6 @@ from api.models import Course, Lesson, Lecture
 admin.site.register(Course)
 admin.site.register(Lesson)
 admin.site.register(Lecture)
+admin.site.register(Forum)
+admin.site.register(Topic)
+admin.site.register(Event)
